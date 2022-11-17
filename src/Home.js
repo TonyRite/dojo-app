caee7db8fb2bfbd3d562b06e8e3ce76991402449
lioswin <lioswinrite@gmail.com>
2022-11-17 18:32:02 +0300

    function as props
    hard

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,18 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Tony', id: 3 }
       ]);
+    
+      const handleDelete = (id)=>{
+      const newBlogs = blogs.filter(blog => blog.id !== id);
+      setBlogs(newBlogs);
+      }
+
     return ( 
         <div className="home">
-             <BlogList blogs={ blogs } title = 'All Blogs!'/>
+             <BlogList blogs={ blogs } title = 'All Blogs!' handleDelete = {handleDelete}/>
+             {/* <BlogList blogs={ blogs.filter((blog)=>blog.author == 'Tony') } title = "Tony's blogs!"/> */}
          </div>
      );
 }
