1fc21cd13028c1d9ebaf3d8a6fff8ff4beb6cb9c
lioswin <lioswinrite@gmail.com>
2022-11-18 13:07:19 +0300

    custom hooks ,error handling

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,14 @@
-import {  useState } from 'react'
+import {  useEffect, useState } from 'react'
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 const Home = () => {
- 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Tony', id: 3 }
-      ]);
-    
-      const handleDelete = (id)=>{
-      const newBlogs = blogs.filter(blog => blog.id !== id);
-      setBlogs(newBlogs);
-      }
+  const { data, isPending, error} = useFetch('http://localhost:8000/blogs');
 
     return ( 
         <div className="home">
-             <BlogList blogs={ blogs } title = 'All Blogs!' handleDelete = {handleDelete}/>
+            {error && <div>{ error }</div>}
+            { isPending && <div>loading......</div>}
+            {data && <BlogList blogs={ data } title = 'All Blogs!'/> }
              {/* <BlogList blogs={ blogs.filter((blog)=>blog.author == 'Tony') } title = "Tony's blogs!"/> */}
          </div>
      );
